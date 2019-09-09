41a067c50785a5d50700a675e3f08fa8e5eef134
Sigfred Balatan Jr <sigfredbalatanjr.@Sigfreds-MacBook-Pro-2.local>
2019-09-09 17:40:55 +0800

    circleci integration

--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	C "github.com/Tech4GoodPH/go-sample-api.git/cmd"
 )
-
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, C.HomeMessage())
-}
 
 func main() {
 	C.StartServer()
